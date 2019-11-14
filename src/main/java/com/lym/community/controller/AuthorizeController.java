39dacb04f801c047703a2a90b94bb2dceddf1842
lym <lym580231@163.com>
2019-11-14 23:19:00 +0800

    mybatis、mysql连接。 github授权登录后将用户信息存入数据库中。

--- src/main/java/com/lym/community/controller/AuthorizeController.java
+++ src/main/java/com/lym/community/controller/AuthorizeController.java
@@ -2,19 +2,27 @@ package com.lym.community.controller;
 
 import com.lym.community.dto.AccessTokenDto;
 import com.lym.community.dto.GithubUser;
+import com.lym.community.mapper.UserMapper;
+import com.lym.community.model.User;
 import com.lym.community.provider.GithubProvider;
+import com.sun.deploy.net.HttpResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
 
 @Controller
 public class AuthorizeController {
 
     @Autowired
     private GithubProvider githubProvider;
-
+    @Autowired
+    private UserMapper userMapper;
     @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
@@ -22,8 +30,11 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
 
+
+
     @RequestMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
+                           HttpServletRequest request) {
         AccessTokenDto accessTokenDto = new AccessTokenDto();
         accessTokenDto.setClient_id(clientId);
         accessTokenDto.setClient_secret(clientSecret);
@@ -31,9 +42,23 @@ public class AuthorizeController {
         accessTokenDto.setRedirect_uri(redirectUri);
         accessTokenDto.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDto);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser != null) {
+            //登录成功
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        } else {
+            //重新登录
+            return "redirect:/";
+
+        }
     }
 
 }
---
+++ src/main/java/com/lym/community/mapper/UserMapper.java
@@ -0,0 +1,15 @@
+package com.lym.community.mapper;
+
+
+import com.lym.community.model.User;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserMapper {
+
+    @Insert("INSERT INTO user(name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId}," +
+            "#{token},#{gmtCreate},#{gmtModified})")
+    void insert(User user);
+
+}
