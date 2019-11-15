fd612a24caa4c2041089f2ddb84afa53339d635a
lym <lym580231@163.com>
2019-11-15 22:44:56 +0800

    登录后用cookie保存用户信息

--- src/main/java/com/lym/community/controller/HomeController.java
+++ src/main/java/com/lym/community/controller/HomeController.java
@@ -1,15 +1,38 @@
 package com.lym.community.controller;
 
+import com.lym.community.mapper.UserMapper;
+import com.lym.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class HomeController {
 
+    @Autowired
+    private UserMapper userMapper;
+
     @RequestMapping("/")
-    public String index(){
+    public String index(HttpServletRequest request) {
+
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")){
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if(user != null){
+                    request.getSession().setAttribute("user",user);
+                }
+                break;
+            }
+        }
+
+
         return "index";
     }
 
--- src/main/java/com/lym/community/mapper/UserMapper.java
+++ src/main/java/com/lym/community/mapper/UserMapper.java
@@ -4,6 +4,7 @@ package com.lym.community.mapper;
 import com.lym.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
@@ -12,4 +13,6 @@ public interface UserMapper {
             "#{token},#{gmtCreate},#{gmtModified})")
     void insert(User user);
 
+    @Select("SELECT id,name,account_id,token,gmt_create,gmt_modified from user where token = #{token}")
+    User findByToken(String token);
 }
