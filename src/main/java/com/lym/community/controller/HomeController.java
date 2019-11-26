52c1238f76301bf7e09f0527ae753e91e2324f36
lym <lym580231@163.com>
2019-11-26 21:18:27 +0800

    Interceptor拦截器的使用

--- src/main/java/com/lym/community/controller/HomeController.java
+++ src/main/java/com/lym/community/controller/HomeController.java
@@ -1,9 +1,6 @@
 package com.lym.community.controller;
 
 import com.lym.community.dto.PageDto;
-import com.lym.community.dto.QuestionDto;
-import com.lym.community.mapper.UserMapper;
-import com.lym.community.model.User;
 import com.lym.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -11,40 +8,21 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @Controller
 public class HomeController {
-
-    @Autowired
-    private UserMapper userMapper;
 
     @Autowired
     private QuestionService questionService;
 
     @RequestMapping("/")
     public String index(HttpServletRequest request, Model models,
-                        @RequestParam(name = "page",defaultValue = "1") Integer page,
-                        @RequestParam(name = "size",defaultValue = "3") Integer size) {
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "3") Integer size) {
 
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    User user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
-        PageDto pageDto = questionService.findAllQuestion(page,size);
-        models.addAttribute("pageDto",pageDto);
+        PageDto pageDto = questionService.findAllQuestion(page, size);
+        models.addAttribute("pageDto", pageDto);
         return "index";
     }
 
